9b15f8cfdfd0cbb6173f6ffbb049f66a4ea1fad5
Mike Criollo <mkcriollo@gmail.com>
2021-01-05 17:25:41 -0500

    CStart tabs

--- skeleton/frontend/tabs.jsx
+++ skeleton/frontend/tabs.jsx
@@ -3,12 +3,55 @@ import React from 'react';
 class Tabs extends React.Component{
     constructor(props){
         super(props);
-        this.props
+
+        this.state = {
+            selectedTab: 0
+        }
+    }
+
+    handleclick(num){
+        this.setState({selectedTab: num})
     }
 
     render(){
-        return ( <div> hello </div> )
+        let title = this.props.arr[this.state.selectedTab].title
+        let content = this.props.arr[this.state.selectedTab].content
+        
+
+        return (
+            
+            <div>
+                <h1>
+                    tabs
+                </h1>
+                <Headers 
+                handleclick={() => this.handleclick()}
+        
+                />
+                <div>
+                    {content}
+                </div>
+            </div>
+        )
     }
 }
 
+class Headers extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        let selected = this.selectedTab
+        return (
+            <div>
+                <h1 onClick={this.handleclick}>one</h1>
+                <h1 onClick={this.handleclick}>two</h1>
+                <h1 onClick={this.handleclick}>three</h1>
+            </div>
+        )
+    }
+
+
+}
 export default Tabs
